9049c98a2c46ab2452838e85f470fa46af498e24
Yujie Zhu <yzhu@Yujies-MBP.lan>
2024-02-23 00:11:10 -0500

    configed local db, add stopwords to keywords

--- config/db_config.py
+++ config/db_config.py
@@ -5,7 +5,7 @@ REDIS_DB_HOST = "127.0.0.1"  # your redis host
 REDIS_DB_PWD = os.getenv("REDIS_DB_PWD", "")  # your redis password
 
 # mysql config
-RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "A23417!wobulijieya!ug")  # your relation db password
+RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "password")  # your relation db password
 RELATION_DB_URL = f"mysql://root:{RELATION_DB_PWD}@localhost:3306/momo_research_db"
 
 # save data to database option
